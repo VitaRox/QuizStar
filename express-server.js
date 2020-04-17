162ba799c5874773879904d48ebe7fa5558c99bf
Derek Hendrick <phantom.dev.elb@gmail.com>
2020-04-16 18:34:34 -0700

    Create express-server.js

---
+++ express-server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const port = 3200;
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+/*
+app.get('/api/dish', cors(), (req, res) => {
+    //const dishes =  getDish();
+    //console.log('api.get dishes -',dishes);
+    //res.json(dishes);
+    var query = req.params.query;
+    Dish
+    .find({'request': query}, function(err, result)
+        {if (err) throw err;
+        if (result) {
+            res.json(result)
+        } else ( res.send(JSON.stringify({error: 'Error'})))})
+    .select({id: 1, name: 1, description: 1});  //chose which fields you want
+})
+
