01b5641be77920110c113dbb65b96d52b038e6ef
Derek Hendrick <phantom.dev.elb@gmail.com>
2020-04-29 15:02:53 -0700

    Adds basic outline for express app.

--- express-server.js
+++ express-server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
 
-const port = 3200;
+const port = 3000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
-/*
-app.get('/api/dish', cors(), (req, res) => {
-    //const dishes =  getDish();
-    //console.log('api.get dishes -',dishes);
-    //res.json(dishes);
-    var query = req.params.query;
-    Dish
-    .find({'request': query}, function(err, result)
-        {if (err) throw err;
-        if (result) {
-            res.json(result)
-        } else ( res.send(JSON.stringify({error: 'Error'})))})
-    .select({id: 1, name: 1, description: 1});  //chose which fields you want
+app.get('/', function (req, res) {
+  res.send('Hello World!')
 })
-*/
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
