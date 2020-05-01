a4ac086c4ece63724af1064d48c519b91538a758
Vita <wiebetc@gmail.com>
2020-05-01 13:29:59 -0700

    - Incorporate Luca's Routing code into React App.js;

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div className="demo-big-content">
+      <Router>
+        <Layout>
+
+          <Header className="header_Color" title="QuizStar" scroll>
+            <Navigation>
+              <Link to="/Login">Login</Link>
+              <Link to="/Profile">Profile</Link>
+              <Link to="/CreateQuiz">Create Quiz</Link>
+              <Link to="/TakeQuiz">Take Quiz</Link>
+
+            </Navigation>
+          </Header>
+          <Drawer title="QuizStar">
+            <Navigation>
+              <Link to="/Login">Login</Link>
+              <Link to="/Profile">Profile</Link>
+              <Link to="/CreateQuiz">Create Quiz</Link>
+              <Link to="/TakeQuiz">Take Quiz</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div>
+              <Route path="/" exact strict component={Home}/>
+              <Route path="/Login" exact strict component={Login}/>
+              <Route path="/Profile" exact strict component={Profile}/>
+              <Route path="/CreateQuiz" exact strict component={QuizCreate}/>
+              <Route path="/TakeQuiz" exact strict component={QuizTake}/>
+            </div>
+          </Content>
+        </Layout>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
