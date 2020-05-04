e694b0618001378d7eb433cce0ae4bdec3abcb35
Derek Hendrick <phantom.dev.elb@gmail.com>
2020-05-03 20:02:04 -0700

    Adds necessary routes

--- backend/routes.js
+++ backend/routes.js
@@ -32,4 +32,45 @@ router.get('/', (req, res) => {
 })
 
 
+//Derek: I'm adding a bunch of routes to match up with our service contract.
+// Grabs all quizzes, using the page ID to choose which ones
+router.get('/quizzes/page/:id', (req, res) => {
+
+})
+
+//Only grabs one particular quiz based on ID
+router.get('/quizzes/:id', (req, res) => {
+
+})
+
+//Posts a new quiz with an automatic ID
+router.post('/quizzes', (req, res) => {
+
+})
+
+//Removes this quiz from the database
+router.delete('/quizzes/:id', (req, res) => {
+
+})
+
+//Allows update of the quiz
+router.put('/quizzes/:id', (req, res) => {
+
+})
+
+//Creates a new user using provided info
+router.post('/users', (req, res) => {
+
+})
+
+//Logs user in
+router.get('/users/login', (req, res) => {
+
+})
+
+//Logs user out
+router.post('/users', (req, res) => {
+
+})
+
 module.exports = router
