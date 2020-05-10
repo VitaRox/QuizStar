be1dfc9214dbef4f82c5196c2d56464f50e351cc
luca <luca627@comcast.net>
2020-05-10 14:51:16 -0700

    upload progress bar, attempt file retrieval to public folder

--- client/src/Components/Profile/index.js
+++ client/src/Components/Profile/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import "../App/App.css";
+import React from "react";
 
+import FileUpload from "./FileUpload.js";
 
+const Profile = () => {
+  return (
+    <div className="container mt-4">
+      <h4 className="display-4 text-center mb-4">schyas</h4>
+      <h1>Profile page Coming Soon!</h1>
 
-const Profile= ()=> {    
-    return(
-        <div>
-            <main>
-                <h1>Profile page Coming Soon!</h1>    
-                <input type="file"></input>
-            </main>
-        </div>
-        )
-}
+      <FileUpload />
+    </div>
+  );
+};
 export default Profile;
