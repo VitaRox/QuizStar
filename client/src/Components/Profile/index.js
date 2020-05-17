da64a6860353c44b7af743aea6fb157cf9b23d65
luca <luca627@comcast.net>
2020-05-17 14:42:24 -0700

    simple photo grabber

--- client/src/Components/Profile/index.js
+++ client/src/Components/Profile/index.js
@@ -1,4 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useDropzone } from "react-dropzone";
+import "../App/App.css";
+
+function Profile() {
+  const [files, setFiles] = useState([]);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
+
+  const images = files.map((file) => (
+    <div key={file.name}>
+      <div>
+        <img src={file.preview} style={{ width: "200px" }} alt="preview" />
+      </div>
+    </div>
+  ));
+
+  return (
+    <div className="App">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p>Drop files here</p>
+      </div>
+      <div>{images}</div>
+    </div>
+  );
+}
+
+export default Profile;
+
+/* import React from 'react';
 import "../App/App.css";
 
 
@@ -12,4 +53,4 @@ const Profile= ()=> {
         </div>
         )
 }
-export default Profile;
+export default Profile; */
