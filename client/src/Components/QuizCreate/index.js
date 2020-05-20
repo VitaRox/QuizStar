d1f60654cdc8047e931243313ee2355185816fdf
Derek Hendrick <phantom.dev.elb@gmail.com>
2020-05-19 17:17:06 -0700

    Makes it so multiples of a component can be used on page

--- client/src/Components/QuizCreate/index.js
+++ client/src/Components/QuizCreate/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
+var numberOfQuestions = 1;
 
-const QuizCreate= ()=> {    
+function addQuestion(){
+	console.log(Math.random());
+}
+
+function test(numberOfQuestions){
+	var elements = [];
+	for(var n = 0; n < numberOfQuestions; n++){
+		elements.push(
+			<div>
+				<form action="">
+					<span>Question {n}</span><br/>
+					<input type="text" id="fname" name="fname" value="John"/><br/>
+					<input type="text" id="lname" name="lname" value="Doe"/><br/>
+				</form>
+				<button id="addQuestion" onclick="addQuestion()">Add Question</button>
+			</div>
+			);
+	}
+	return(
+		<div>
+			{elements}
+		</div>
+		)
+}
+
+const QuizCreate= ()=> {  
     return(
         <div>
             <main>
-                <h1>Quiz Creating page Coming Soon!</h1>    
+            	<p>Number of Questions: {numberOfQuestions}</p>
+                {test(1)}
             </main>
         </div>
         )
