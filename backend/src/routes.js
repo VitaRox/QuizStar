89c7ee8842e3e051bdf860fb696fb0350e3fd4bf
YAG0731 <57081128+YAG0731@users.noreply.github.com>
2020-05-23 15:33:53 -0700

    models contains db schema, routes create question

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,30 +1,48 @@
 const express = require('express')
 const router = express.Router()
 
-// get all quiz questions
-router.get('/quizpool', (req, res) => {
+module.exports = router
 
-})
+const Quizpool = require('./models/quizPool')
+
 
 // get one quiz question
 router.get('/quizpool/:id', (req, res) => {
+    try {
+        const _id = req.params.id 
+
+        const question = await Question.findOne({_id})        
+        if(!question){
+            return res.status(404).json({})
+        }
+        else{
+            return res.status(200).json(question)
+        }
+    } catch (error) {
+        return res.status(500).json({"error":error})
+    }
 
 })
 
 // create one quiz question
 router.post('/quizpool', (req, res) => {
+    try {
+        const { questionText } = req.body
+        const { options } = req.body
+        const { solution } = req.body
 
+        const question = await Question.create({
+            questionText,
+            options,
+            solution
+        })
+
+        return res.status(201).json(question)
+    } catch (error) {
+        return res.status(500).json({"error":error})
+    }
 })
 
-// update one quiz question
-router.put('/quizpool/:id', (req, res) => {
-
-})
-
-// delete one quiz question
-router.delete('/quizpool/:id', (req, res) => {
-
-})
 
 // Testing
 router.get('/', (req, res) => {
