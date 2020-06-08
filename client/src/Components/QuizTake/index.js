dcd0184c3e7c80d6e2c6529f6a2d0313831555e7
Jennvillacis <jennvillacis@gmail.com>
2020-06-07 21:03:49 -0700

    Update index.js

--- client/src/Components/QuizTake/index.js
+++ client/src/Components/QuizTake/index.js
@@ -22,17 +22,24 @@ export default class QuizView extends Component {
   computeAnswer = (answer, correctAnswer) => {
     if(answer === correctAnswer) {
       this.setState({
-        score: this.setState.score + 1
+        score: this.state.score + 1
       });
     }
     this.setState({
       responses: this.state.responses < 5 ? this.state.responses + 1 : 5
     });
   };
-playAgain= () => {
-  this.getQuestions
 
-}
+
+playAgain = () => {
+  this.getQuestions();
+  this.setState({
+    score:0,
+    responses:0
+  });
+};
+
+
 componentDidMount() {
   this.getQuestions();
 }
@@ -41,7 +48,7 @@ componentDidMount() {
     return(
       <div className="quiz-container">
         <div className="title">Quiz</div>
-        {this.state.questionBank.length >0 &&
+        {this.state.questionBank.length > 0 &&
           this.state.responses < 5 &&
           this.state.questionBank.map(
           ({question, answers, correct, questionId}) => (
@@ -53,6 +60,7 @@ componentDidMount() {
             />
           )
            )}
+
            {this.state.responses === 5 ? (
              <Result score={this.state.score} playAgain={this.playAgain}/>
            ): null}
