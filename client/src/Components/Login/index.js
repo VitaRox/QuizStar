489992c2dba4acaf8f53fa3fb2a3dfad6dd44d68
Vita <wiebetc@gmail.com>
2020-06-08 14:08:14 -0700

    - Basic rendering form; - Doesn't send data yet; not sure why;

--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import PasswordForm from '../LoginComponents/PasswordForm';
 import UsernameForm from '../LoginComponents/UsernameForm';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {database} from './MiniTestJsonDB.js';
 
 // This component represents the overall login form the
 // users uses to submit their credentials through;
@@ -12,14 +13,14 @@ function Login() {
   const [error, setError] = useState(false);
   // isLoaded lets the front-end know that the user is now successfully logged in;
   // If user is logged in, unnecessary to display login forms;
-  const [isLoaded, setLoaded] = useState(true);
+  const [isLoaded, setLoaded] = useState(false);
 
   // The following 2 represent the info the user inputs in order to login
   const [usernameInput, setUsernameInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
 
   // 'Payload' represents the array of data that the db will return upon successful (!error) condition;
-  // const [payload, setPayload] = useState([]);
+  const [payload, setPayload] = useState([]);
 
   // The following two consts will update and store the creds the user enters;
   const username = () => {
@@ -30,14 +31,14 @@ function Login() {
   };
 
   // This const refers to the url of the service we are contacting;
-  const apiURL = "https://localhost:8080";
+  const apiURL = "https://localhost:8080/";
 
-  // useEffect updates the look/behavior of this component upon updates to
-  // user login data, which is signalled by onClick;
-  // Sends login data back to our API, which in turn attempts
-  // to fetch said data from MongoDB;
-  useEffect(() => {
-    const response = fetch(apiURL, {
+  // Our click handler for login-credentials-submission event;
+  // Sends the user input data to API for validation and retrieval
+  // of related account info, if successful;
+  // Updates all related state Hooks;
+  const handleClick = (() => {
+    const response = () => fetch(apiURL, {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
       mode: 'cors', // no-cors, *cors, same-origin
       headers: new Headers ({
@@ -47,23 +48,30 @@ function Login() {
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(username, password) // body data type must match "Content-Type" header
     })
-    .then((response) => response.json()); // parses JSON response into native JavaScript objects
+      .then((response) => response.json()) // parses JSON response into native JavaScript objects
+      .then(setPayload(response))
+      .then(requestUserInfo(response));
   });
 
   // This method retrieves server response with the provided token;
   function requestUserInfo(token) {
-    return fetch(`${apiURL}/signin`, {
+    return fetch(`${apiURL}/signin?${username===database.username};${passwordInput===database.password}`, {
       method: 'GET',
       headers: new Headers({
         Authorization: `Bearer ${token}`,
       })
     })
-      .then((response) => response.json());
-
+      .then((response) => console.log(response.json()));
   }
+  // useEffect updates the look/behavior of this component upon updates to
+  // user login data, which is signalled by onClick;
+  // Sends login data back to our API, which in turn attempts
+  // to fetch said data from MongoDB;
+  useEffect(handleClick)
+
   // This is used to display the login forms in this view if the user has
   // not logged in yet, i.e. if !isLoaded;
-  const login = () => {
+  function showLoginForms(){
     if(!isLoaded) {
       return <div>
         <UsernameForm />
@@ -72,27 +80,12 @@ function Login() {
     }
   };
 
-  // Our click handler for login-credentials-submission event;
-  // Sends the user input data to API for validation and retrieval
-  // of related account info, if successful;
-  // Updates all related state Hooks;
-  const handleClick = () => {
-
-    // Update loginCreds state array with user input;
-    // The individual components handle onSubmit/values entered,
-    // so no need for it here;
-    setLoginCreds([<UsernameForm username />, <PasswordForm password />]);
-
-    // Use 'login' to re-render login components (if login unsuccessful),
-    // sweep them away if login is successful;
-    login();
-  };
 
   return(
       <div>
           <main>
               <h1>Use the forms below to submit yr login credentials, human.</h1>
-                  {/*{window.onchange=login()}*/}
+                  {window.onchange=showLoginForms()}
 
                   {/*
                     Button below will submit all info entered to find user account/data,
