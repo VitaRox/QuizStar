376a8cc798cfe7f3cdb5614661140d8fd2fcc6a1
Vita <wiebetc@gmail.com>
2020-06-08 19:34:02 -0700

    - Installs axios, bootstrap, and something else I am forgetting at the moment; - Cleans up old nonsense code; - Moves showLoginForms() function (to update whether username and password forms are displayed or not) into inside useEffect, 	that it may be called every time the status of 'isLoaded' is changed; - Once user types creds into forms, clicking the submit button will call setCredentials(), which in turn checks that blank forms aren't being passed 	and if not, calls setUsernameInput to set local prop 'usernameInput' = what the user has typed into that form, and the same 	for calling setPasswordInput to update the state of passwordInput to be = PasswordForm.password;

--- client/src/Components/Login/index.js
+++ client/src/Components/Login/index.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import PasswordForm from '../LoginComponents/PasswordForm';
 import UsernameForm from '../LoginComponents/UsernameForm';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {database} from './MiniTestJsonDB.js';
+import axios from 'axios';
 
 // This component represents the overall login form the
 // users uses to submit their credentials through;
@@ -15,50 +15,58 @@ function Login() {
   // If user is logged in, unnecessary to display login forms;
   const [isLoaded, setLoaded] = useState(false);
 
-  // The following 2 represent the info the user inputs in order to login
-  const [usernameInput, setUsernameInput] = useState("");
-  const [passwordInput, setPasswordInput] = useState("");
-
   // 'Payload' represents the array of data that the db will return upon successful (!error) condition;
   const [payload, setPayload] = useState([]);
-
-  // The following two consts will update and store the creds the user enters;
-  const username = () => {
-    setUsernameInput(passwordInput);
-  };
-  const password = () => {
-    setPasswordInput(passwordInput);
-  };
-
-  // This const refers to the url of the service we are contacting;
-  const apiURL = "https://localhost:8080";
+  // The following 2 Hooks are used to track the state of the username/password fields,
+  // i.e. whether they have been filled in
+  const [usernameInput, setUsernameInput] = useState(null);
+  const [passwordInput, setPasswordInput] = useState(null);
 
   // This is used to display the login forms in this view if the user has
   // not logged in yet, i.e. if !isLoaded;
-  function showLoginForms(){
-    if(!isLoaded) {
+  function showLoginForms() {
+    if (!isLoaded) {
       return <div>
-        <UsernameForm />
-        <PasswordForm />
+        <UsernameForm/>
+        <PasswordForm/>
       </div>
     }
   };
 
+  // Upon change, update the window (whether or not to display login submission forms);
+  useEffect(window.onchange = showLoginForms());
 
-  return(
-      <div>
-          <main>
-              <h1>Use the forms below to submit yr login credentials, human.</h1>
-                  {window.onchange=showLoginForms()}
+  /*
+   Called when submit is clicked;
+   Checks whether user is attempting to pass blank forms;
+   If not, calls State methods to setState of appropriate props
+   = the props exported by the appropriate React components;
+  */
+  function setCredentials() {
+    while (UsernameForm.username != null || PasswordForm.password != null) {
+      alert("Cannot be blank.");
+    }
+    setUsernameInput(UsernameForm.username);
+    setPasswordInput(PasswordForm.password);
+  }
 
-                  {/*
-                    Button below will submit all info entered to find user account/data,
-                    error if not;
-                    */}
-                  <button id="submitLoginCreds" type="submit">Submit login credentials.</button>
-          </main>
-      </div>
-      );
-};
+  // This const refers to the url of the API service we are contacting;
+  // Should map to our Express server, server.js;
+  const API_URL = "https://localhost:3000/login/:submit";
 
+  return (
+    <div>
+      <main>
+        <h1>Use the forms below to submit yr login credentials, human.</h1>
+
+        {/*
+          Button below will submit all info entered to find user account/data,
+          error if not;
+        */}
+        <button id="attemptLogin" type="submit" onSubmit={setCredentials()}>Submit login credentials.</button>
+      </main>
+    </div>
+  );
+
+}
 export default Login;
