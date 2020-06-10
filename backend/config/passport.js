93781f00f73fb618dbb6999381ea6f48e437ec55
YAG0731 <57081128+YAG0731@users.noreply.github.com>
2020-06-09 19:10:35 -0700

    working route

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
@@ -17,3 +18,27 @@ passport.use(new LocalStrategy({
       return done(null, user);
     }).catch(done);
 }));
+=======
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const keys = require("../config/keys");
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
+};
+>>>>>>> Stashed changes
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const mongoose = require('mongoose');
 const express = require('express');
 var cors = require('cors');
@@ -33,3 +34,34 @@ app.use(logger('dev'));
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+=======
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+
+const app = express();
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose.connect(db,{ useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+  
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+const port = process.env.PORT || 3200;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+>>>>>>> Stashed changes
