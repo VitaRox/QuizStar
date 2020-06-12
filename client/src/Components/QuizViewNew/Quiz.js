ebf80ae118ca59760885b4fa6cdf6ed4d756a24e
Jennvillacis <jennvillacis@gmail.com>
2020-06-12 14:48:50 -0700

    edits

--- client/src/Components/QuizViewNew/Quiz.js
+++ client/src/Components/QuizViewNew/Quiz.js
@@ -1,13 +1,9 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import QuestionBox from "./QuestionBox";
-import Result from "./Results";
 
 export default class Quiz extends Component{
   state = {
-      quiz: [],
-      score:0,
-      responses:0
+      quiz: []
   };
 
 componentDidMount = () => {
@@ -26,47 +22,22 @@ getQuestions = () =>{
       });
  };
 
-computeAnswer = (answer, correctAnswer) => {
-    if(answer === correctAnswer) {
-      this.setState({
-        score: this.state.score + 1
-      });
-    }
-    this.setState({
-      responses: this.state.responses < this.state.questions ? this.state.responses + 1 : this.state.questions
-    });
-  };
-
-  playAgain = () => {
-  this.getQuestions();
-  this.setState({
-    score:0,
-    responses:0
-  });
-};
-
-
-
 
 render()
 {
          return (
-           <div className="quiz-container">
-        {this.state.quiz.map(item =>
-          {item.answers.map(subItem =>
-            <QuestionBox
-            key={subItem.question}
-            question={subItem.question}
-            options={subItem.answers}
-            />
-            )}
-          )}
-
-
-           {this.state.responses === this.state.questions ? (
-             <Result score={this.state.score} playAgain={this.playAgain}/>
-           ): null}
-      </div>
+           <div>
+           {this.state.quiz.map((item)=>
+               <div>
+               <h3>{item.quizName}</h3>
+               <ul>
+               {item.answers.map((sub)=>
+                 <li>{sub.question}</li>
+               )}
+               </ul>
+               </div>
+             )}
+          </div>
        )
      }
 }
