fd25888e6627e87c8415ecb46043e4f6de95b23f
Vita <wiebetc@gmail.com>
2020-06-16 16:10:19 -0700

    - This commit makes the page actually display again and appears to fix the Req Header Too Big error (431); - I did this by changing App.jsx back to App.js, and by adding a "homepage" attribute to my client/package.json, which 	appears to give the /%PUBLIC_URL%/ thing something to point to;

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/App/App.jsx'
+import App from './Components/App/App.js'
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
